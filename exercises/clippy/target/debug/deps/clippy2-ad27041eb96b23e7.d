e94263779e3fe2456380366ce4ad5a9f4bf71e10
MateoRH <mateo.1459@hotmail.com>
2022-06-20 12:51:11 -0500

    all exercises done

---
+++ exercises/clippy/target/debug/deps/clippy2-ad27041eb96b23e7.d
@@ -0,0 +1,7 @@
+/Users/mateorh/Documents/Rust/rustlings/exercises/clippy/target/debug/deps/clippy2-ad27041eb96b23e7.rmeta: clippy2.rs
+
+/Users/mateorh/Documents/Rust/rustlings/exercises/clippy/target/debug/deps/clippy2-ad27041eb96b23e7.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ exercises/option/option2.rs
@@ -0,0 +1,23 @@
+// option2.rs
+// Make me compile! Execute `rustlings hint option2` for hints
+
+
+fn main() {
+    let optional_word = Some(String::from("rustlings"));
+    if let Some(word) = optional_word {
+        println!("The word is: {}", word);
+    } else {
+        println!("The optional word doesn't contain anything");
+    }
+
+    let mut optional_integers_vec: Vec<Option<i8>> = Vec::new();
+    for x in 1..10 {
+        optional_integers_vec.push(Some(x));
+    }
+
+    while let Some(integer) = optional_integers_vec.pop() {
+        if let Some(x) = integer {
+            println!("current value: {}", x);
+        }
+    }
+}
